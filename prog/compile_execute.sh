4504c3e52a6b3acf3f9116e0c5372ca42643ea8f
lm687 <lm687@cam.ac.uk>
2019-11-11 15:52:18 +0000

    bug in mutations from sigs

---
+++ prog/compile_execute.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+mkdir -p output_data/BirthRateGrids
+mkdir -p output_data/DriverGrids
+mkdir -p output_data/PopsGrids
+mkdir -p output_data/PassengersGrids
+mkdir -p output_data/MigrationRateGrids
+
+#g++ -o demon demon.cpp -I/usr/local/include/boost/ -lm #compile
+g++ -o demon demon_sig.cpp -I/usr/local/include/boost/ -lm #compile
+
+./demon  ./ init_conf_file.dat ##output_data #execute
+
+printf "\nEnd of execution\n"
